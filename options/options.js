ae8b118dbda3a7bda5163485083421d25a9f389b
Crowdbotics <team@crowdbotics.com>
2024-02-21 20:41:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steel-city-financia-46550",
+  url: "https://steel-city-financia-46550.botics.co",
+  api: "https://steel-city-financia-46550.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
